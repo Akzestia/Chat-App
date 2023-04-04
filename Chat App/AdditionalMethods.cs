6ad8ee2fda3aaf0c4848dfbfd051a104a68c1788
Azure Dust <azurexsx@gmail.com>
2023-04-04 18:19:25 +0200

    Added classes

---
+++ Chat App/AdditionalMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Chat_App
+{
+    public static class AdditionalMethods
+    {
+        public static string ToSHA256(string password_string) //Hashing user password with sha256   
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password_string);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+}
---
+++ Chat App/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_App
+{
+    public class User // User class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public User() {}
+
+        public User(int id, string name, string password)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+        }
+    }
+}
